5b981935f5919e075c0f621a53f7ce716d1c3e5d
Yakub Juma <qoub@Yakubs-MacBook-Air-M2.local>
2025-08-26 16:58:42 +0300

    feature(HealthCheck): Edited App healthcheck end-point

--- src/controllers/healthcheck.js
+++ src/controllers/healthcheck.js
@@ -3,14 +3,15 @@ import { request, response } from "express";
 export const healthCheck = async (req = request, res = response) => {
   try {
     res.status(200).json({
-      status: "OK",
+      status: "Ok",
+      statusCode: 200,
+      message: "Health check successful",
       uptime: process.uptime(), // seconds since server started
       timestamp: Date.now(), // current server time
-      message: "Server is healthy 🚀",
+      message: "Server is healthy",
     });
   } catch (error) {
     res.status(500).json({
-      success: false,
       statusCode: 500,
       message: `Internal server error: ${error}`,
     });
